e9a88d5268b695d6c4932f10745d9c3cc089243e
Nick Simpson <nicholas.simpson@icloud.com>
2015-12-13 00:25:58 -0500

    keyup

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,13 @@
 (function($) {
 
 	$('.container').on('click', function() {
-		$(this).append('<p>hello</p>');
+		$(this).append('<input type="checkbox" name="checkbox" id="checkbox_id" value="value"><input type="text" class="todo-input">');
 	});
 
+	$('.todo-input').keyup(function (e) {
+		if (e.keyCode == 13){
+
+console.log('it works');
+		}
+	});
 })(jQuery);
