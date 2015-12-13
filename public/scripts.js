79f1d27f9da3695d6ac17934504f82b52b818169
Trevor Helms <thelms501@gmail.com>
2015-12-13 01:36:50 -0500

    Draggable elements

--- public/scripts.js
+++ public/scripts.js
@@ -1,17 +1,16 @@
 (function($) {
 
-	var listItem = $('<input type="checkbox" name="checkbox" id="checkbox_id" value="value"><input type="text" class="todo-input">');
+	var listItem = $('<div class="draggable"><input type="checkbox" name="checkbox" id="checkbox_id" value="value"><input type="text" class="todo-input"></div>');
 
 	$('.container').on('click', function() {
-		$(listItem).clone()
-		.keyup(function(e) {
+		var el = $(listItem).clone().appendTo('.container');
+		el.find('input[type="text"]').keyup(function(e) {
 			if (e.keyCode === 13) {
 				var text = $(this).val();
 				$(this).replaceWith('<span>' + text + '</span>');
 			}
 		})
-		.appendTo('.container')
-		.wrap('<div class="draggable"></div>');
+
 		$('.draggable').draggable();
 	});
 
